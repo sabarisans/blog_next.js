55ba031a621efa4f00d1f9a61ae0c3a66eefc7e9
sabarisan-s <iamsabarisan@gmail.com>
2024-10-21 06:53:17 +0530

    tailwind set up removed

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,9 +1,13 @@
-const NotFound = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+import Link from "next/link";
 
-export default NotFound
+const NotFound = () => {
+    return (
+        <div>
+            <h2>Not found</h2>
+            <p>Sorry,the page yor are looking for dose not exist</p>
+            <Link href={"/"}>Return to Home</Link>
+        </div>
+    );
+};
+
+export default NotFound;
